42866fd131c4fd5eb5087dd91780141987aaf9bf
AvilaC <avilajose@live.com>
2015-06-18 23:24:00 +0200

    Créditos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -9,3 +9,6 @@ exports.answer = function(req,res){
 	    res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
+exports.author=function(req, res){	
+	res.render('author', { creditos: "Autor: J. Ávila"});
+};
