f7f661dcca7ebb0f2823ea7cf923af30788578f1
AvilaC <avilajose@live.com>
2015-06-19 16:11:19 +0200

    Crear pregunta

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,8 @@ router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/quizes?search:texto_a_buscar', quizController.answer);
+router.get('/quizes/new',quizController.new);
+router.post('/quizes/create',quizController.create);
 router.get('/author',quizController.author);
 
 module.exports = router;
